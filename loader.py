Fix frequency calculation, parameters choice

loader.py
@@ -15,9 +15,9 @@ NEGATIVE_SAMPLE_PROB = 0.87   # Probability of a negative sample
 MAX_LEN = 15_000 # Number of words in vocabulary
 BATCH_SIZE = 200
 # Skipping words  double the execution time
-THREESHOLD = 1e-3
-BASE_SKIP = 1.2 # Increase for skipping fewer words (default is 1.2)
-OFFSET_SKIP = 8 # Increase for skipping fewer words  (default is 8)
+THRESHOLD = 3e-5 # Never skip below this threshold
+BASE_SKIP = 1.9 # Increase for skipping more words (default is 1.2, always greater than 1)
+OFFSET_SKIP = 0.025 # Decrease for skipping more words  (default is 0.00358)
 
 #======================================================
 
@@ -197,8 +197,8 @@ class NGramBatchIterator:
     def skip_too_frequent(self, word):
         """Skip words with too many occurrency"""
         freq = self.word_frequency[word]
-        if freq > THREESHOLD:
-            ratio = 1e-3 / freq * BASE_SKIP**(np.log(freq)+OFFSET_SKIP)
+        if freq > THRESHOLD:
+            ratio = np.clip(OFFSET_SKIP / freq * BASE_SKIP ** (np.log(freq)), 0, 1)
             r = np.random.random()
             if r > ratio:
                 return True

word_frequency.py
@@ -5,34 +5,47 @@ import numpy as np
 
 
 # Skipping words  double the execution time
-THREESHOLD = 5e-4
-BASE_SKIP = 1.2 # Increase for skipping fewer words (default is 1.2)
-OFFSET_SKIP = 8 # Increase for skipping fewer words  (default is 8)
+THREESHOLD = 3e-5 # Never skip below this threeshold
+BASE_SKIP = 1.9 # Increase for skipping more words (default is 1.2, always greater than 1)
+OFFSET_SKIP = 0.025 # Decrease for skipping more words  (default is 0.00358)
 wc = train.word_frequency
 freqs = np.array(list(wc.values()))
-ratio = np.clip(THREESHOLD / freqs * BASE_SKIP**(np.log(freqs)+OFFSET_SKIP), 0, 1)
+ratio = np.clip(OFFSET_SKIP / freqs * BASE_SKIP**(np.log(freqs)), 0, 1)
 ratio[freqs < THREESHOLD] = 1
 
 skip_prob = np.sum((1 - ratio) * freqs)
 new_freqs = ratio * freqs
 new_freqs = new_freqs / new_freqs.sum()
-fig, ax = plt.subplots(2, 1, figsize=(5,9))
+fig, ax = plt.subplots(3, 1, figsize=(5,13))
 
-sns.ecdfplot(freqs, ax=ax[0])
-sns.ecdfplot(new_freqs, ax=ax[0])
+old_prob = np.exp(np.linspace(-5.5,-2,100)*np.log(10))
+ratio_prob = np.clip(OFFSET_SKIP / old_prob * BASE_SKIP**(np.log(old_prob)), 0, 1)
+ratio_prob[old_prob < THREESHOLD] = 1
+new_prob = old_prob * ratio_prob / (1 - skip_prob)
+ax[0].plot(old_prob, new_prob)
 ax[0].set_xscale('log')
+ax[0].set_yscale('log')
+ax[0].set_xlabel("Old probability")
+ax[0].set_ylabel("New probability")
+ax[0].set_title("Probability variation after skipping")
 ax[0].grid()
+
+sns.ecdfplot(freqs, ax=ax[1], complementary=False)
+sns.ecdfplot(new_freqs, ax=ax[1], complementary=False)
+ax[1].set_xscale('log')
+# ax[1].set_yscale('log')
+ax[1].grid()
 ax[1].set_xlabel("Probability to be chosen")
 print(f"Probability to skip a word is {skip_prob*100:.1f}%. Number of iteration are "
       f"increased of {1/(1-skip_prob)*100 - 100:.0f}%")
 
-
-ax[1].plot(np.linspace(0, 1, len(freqs))*100, np.cumsum(freqs[::-1])*100, label="base")
-ax[1].plot(np.linspace(0, 1, len(new_freqs))*100, np.cumsum(new_freqs[::-1])*100, label="skip words")
-ax[1].set_xlabel("Ratio of words (%)")
-ax[1].set_ylabel("Probability to be chosen (%)")
-ax[1].grid()
-ax[1].legend()
+ax[2].plot(np.linspace(0, 1, len(freqs))*100, np.cumsum(freqs[::-1])*100, label="base")
+ax[2].plot(np.linspace(0, 1, len(new_freqs))*100, np.cumsum(new_freqs[::-1])*100, label="skip "
+                                                                                        "words")
+ax[2].set_xlabel("Ratio of words (%)")
+ax[2].set_ylabel("Probability to be chosen (%)")
+ax[2].grid()
+ax[2].legend()
 # ax[1].set_xscale('log')
 # ax[1].set_yscale('log')
 fig.show()