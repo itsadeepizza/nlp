Fix error in masking in model

babyGPT/config.py
@@ -7,7 +7,7 @@ class Config():
 
     root = Path(__file__).resolve().parents[1]
 
-    INTERVAL_TENSORBOARD = 25
+    INTERVAL_TENSORBOARD = 100
     INTERVAL_SAVE_MODEL  = 1000
     RATIO_KEEP_MODEL = 10 # delete old models when saving a new one, but keep a ratio of models
     INTERVAL_UPDATE_LR   = 100
@@ -31,12 +31,8 @@ class Config():
 
     BPE_MODEL  = "dbmdz/bert-base-italian-xxl-cased"
     DEVICE     = torch.device('cpu')
-    MAP_LABEL  = {
-                  'joy'     : 0,
-                  'sadness' : 1,
-                  'anger'   : 2,
-                  'fear'    : 3
-                 }
+
+    # Learning rate
     LR_INIT  = 1E-4
     LR_DECAY = 1E-1
     LR_STEP  = 5e5

babyGPT/main.py
@@ -3,10 +3,8 @@ from config import selected_config as conf
 conf.BATCH_SIZE = 5
 conf.LOAD_PATH  = None
 conf.LOAD_IDX   = 0
-conf.LOAD_PATH = r'runs/models/23-01-15-09H19_old_work'
-conf.LOAD_IDX = 114000
-conf.LOAD_PATH = r'runs/models/23-01-15-02H12_quaint_number'
-conf.LOAD_IDX = 339000
+conf.LOAD_PATH = None
+conf.LOAD_IDX = 0
 conf.DEVICE     = torch.device("cuda")
 conf.set_derivate_parameters()
 from train import Trainer

babyGPT/model.py
@@ -23,7 +23,10 @@ class AttentionLayer(nn.Module):
         x = torch.matmul(Q, torch.transpose(K, -1, -2))
         # index i of Q can see index j of K iff j <= i (masking)
         x = torch.tril(x)
-
+        # Attention! We need to mask previous token, but setting attention equal to 0 is not enough, since softmaw will convert 0 values to positive ones.
+        # A quick solution (probably not the best one) is setting 0 values equals to - inf)
+        # TODO: this is probably not the cleanest way to do it ...
+        x[x == 0] = - torch.inf
         x = x / math.sqrt(self.embedding_dim)
         x = F.softmax(x, dim=-1)
 
@@ -154,10 +157,10 @@ class Transformer(nn.Module):
 
         self.loss = nn.CrossEntropyLoss()
 
-    def forward(self, x, calculate_loss=False):
+    def forward(self, tokenized, calculate_loss=False):
         # y = torch.flatten(x[:, :-1].to(int)) # just a test, predict sentence without shift
-        y = torch.flatten(x[:, 1:].to(int))
-        x = self.embedding(x)
+        y = torch.flatten(tokenized[:, 1:].to(int))
+        x = self.embedding(tokenized)
         x = self.encoder_stack(x)
         x = self.classification_head(x)  # NO SOFTMAX!!
 

babyGPT/test.py
@@ -16,15 +16,15 @@ from transformers import AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained(conf.BPE_MODEL)
 
 if __name__ == '__main__':
-    conf.LOAD_PATH = r'runs/models/23-01-15-02H12_quaint_number'
+    conf.LOAD_PATH = r'runs/models/23-01-15-12H23_clean_week'
     # conf.LOAD_PATH = r'runs/models/23-01-15-09H19_old_work'
-    conf.LOAD_IDX = 339000
+    conf.LOAD_IDX = 439000
     # conf.LOAD_IDX = 114000
     conf.DEVICE = torch.device("cpu")
 
 
     trainer = Trainer(None, None)
 
-    sentence = 'il gatto è'
+    sentence = 'Negli anni cinquanta il convento venne demolito e fu costruita una nuova'
     candidates = trainer.test_sample(sentence, temperature=1.5)
     print(candidates)